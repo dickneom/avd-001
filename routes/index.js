3f2b833cf9f471ec01a95b01b5679e1ca19c2d12
dickneom <dickneom@yahoo.com>
2017-02-22 17:49:38 -0500

    listando vestidos y usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+	console.log('(INDEX.JS) Atendiendo la ruta / GET')
+//	db.Dress.findAll().then (function (dresses) {
+	var dresses = getDresses()
+	console.log('Vestidos recibidos ' + dresses.length);
+		res.render('dresses/dresses', {
+			pageTitle: 'Vestidos',
+			pageName: 'dresses',
+			sessionUser: null,
+			errors: null,
+			dresses: dresses
+		})
+//	}).catch()
+})
+
+var db = require('../models/db')
+
+var getDresses = function () {
+	db.Dress.findAll().then(function (dresses) {
+		console.log('Encontrados los vestidos ' + dresses.length);
+		return dresses
+	})
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('(USERS.JS) Atendiendo la ruta /users/ GET')
+  db.User.findAll().then(function (users) {
+  	res.render('users/users', {
+  		pageTile: 'Lista de Usuarios',
+  		pageName: 'users',
+  		sessionUser: null,
+  		errors: null,
+      users: users
+  	})
+  }).catch()
 });
 
 module.exports = router;
