f191b37e43503234f9bb537458496e3d6c22e7d1
DickNeoM <dickneom@yahoo.com>
2017-03-20 08:45:26 -0500

    Idem

--- routes/dress_images.js
+++ routes/dress_images.js
@@ -74,99 +74,106 @@ router.get('/:dressId([0-9]+)/images', controlSession.isSession, controlDresses.
 /**
 / Medoto POST para la ruta dresses/update, para actualizar la informacion de un vestido
 */
-router.post('/images', controlSession.isSession, uploader.array('image', 6), controlDresses.isOwnerDress, function(req, res, next) {
-    console.log('(DRESS_IMAGES.JS) Atendiendo la ruta /dresses/images POST');
-    console.log('(DRESS_IMAGES.JS) Archivos:', req.files);
+router.post('/images',
+    controlSession.isSession,
+    uploader.array('image', 6, function(error) {
+        console.log('(DRESS_IMAGES.JS) Error al subir las imagenes: ', error);
+    }),
+    controlDresses.isOwnerDress,
+    function(req, res, next) {
+        console.log('(DRESS_IMAGES.JS) Atendiendo la ruta /dresses/images POST');
+        console.log('(DRESS_IMAGES.JS) Archivos:', req.files);
 
-    if (req.files && req.files.length > 0) {
-        var numImagenes = req.files.length;
-        console.log('(DRESS_IMAGES.JS) Numero de archivos:', numImagenes);
+        if (req.files && req.files.length > 0) {
+            var numImagenes = req.files.length;
+            console.log('(DRESS_IMAGES.JS) Numero de archivos:', numImagenes);
 
-        var dressId = req.body.dressId;
-        console.log('(DRESS_IMAGES.JS) Vestido id:', dressId);
-        if (dressId) {
-            db.Dress.findOne({
-                where: {
-                    id: dressId
-                }
-            }).then(function(dress) {
-                console.log('(DRESS_IMAGES.JS) Encontrado el vestido: ', dressId);
-
-                // por cada foto
-                //   Verificar si la foto fue subida
-                //   Verificar que no haya en total mas de 6 fotos
-                //      Una forma: seria eliminar todas las imagenes y cargar las nuevas
-                //      Otra: verificar las imagenes subidas anteriores
-                //   Enviar la fotos a cloudinari
-                //   Agregar las fotos a dresses_photos
-                //
-                // Cambiar el estado el vestido a 1 (REGISTRADO)
-                console.log('(DRESS_IMAGES.JS) IMAGENES');
-                console.log('(DRESS_IMAGES.JS) Mostrando los seis vestidos registrado anteriormente.');
-                var imgUrl;
-                for (var k = 0; k < 6; k++) {
-                    imgUrl = req.body.imageUrl[k];
-                    console.log('(DRESS_IMAGES.JS) Imagen: ', imgUrl, ' k:', k);
-                }
-
-                console.log('(DRESS_IMAGES.JS) Mostrando los seis vestidos ingresados ahora.');
-                for (var i = 0; i < 6; i++) {
-                    if (req.files[i]) {
-                        console.log('Cargando imagenes: ', i, ' - ', req.files[i].path);
-                    } else {
-                        console.log('Cargando imagenes: ', i, ' - ', 'NINGUNA');
+            var dressId = req.body.dressId;
+            console.log('(DRESS_IMAGES.JS) Vestido id:', dressId);
+            if (dressId) {
+                db.Dress.findOne({
+                    where: {
+                        id: dressId
                     }
-                }
+                }).then(function(dress) {
+                    console.log('(DRESS_IMAGES.JS) Encontrado el vestido: ', dressId);
 
-                //                var numImagenes = req.files.length;
-                //                console.log('(DRESS_IMAGES.JS) Mostrando los vestidos añadidos.', numImagenes);
-                //                for (var j = 0; j < numImagenes; j++) {
-                //                    console.log('Cargando imagenes: ', req.files[j].path);
+                    // por cada foto
+                    //   Verificar si la foto fue subida
+                    //   Verificar que no haya en total mas de 6 fotos
+                    //      Una forma: seria eliminar todas las imagenes y cargar las nuevas
+                    //      Otra: verificar las imagenes subidas anteriores
+                    //   Enviar la fotos a cloudinari
+                    //   Agregar las fotos a dresses_photos
+                    //
+                    // Cambiar el estado el vestido a 1 (REGISTRADO)
+                    console.log('(DRESS_IMAGES.JS) IMAGENES');
+                    console.log('(DRESS_IMAGES.JS) Mostrando los seis vestidos registrado anteriormente.');
+                    var imgUrl;
+                    for (var k = 0; k < 6; k++) {
+                        imgUrl = req.body.imageUrl[k];
+                        console.log('(DRESS_IMAGES.JS) Imagen: ', imgUrl, ' k:', k);
+                    }
 
-                /*          cloudinary.uploader.upload(req.file[i].path, function (result) {
-                            var fileUrl = result.url
-                            // var fileSecureUrl = result.secure_url
+                    console.log('(DRESS_IMAGES.JS) Mostrando los seis vestidos ingresados ahora.');
+                    for (var i = 0; i < 6; i++) {
+                        if (req.files[i]) {
+                            console.log('Cargando imagenes: ', i, ' - ', req.files[i].path);
+                        } else {
+                            console.log('Cargando imagenes: ', i, ' - ', 'NINGUNA');
+                        }
+                    }
 
-                            // dress.image = fileUrl
-                            dress.stateId = 1
-                            console.log('********* Vestido a grabar: ' + dress)
+                    //                var numImagenes = req.files.length;
+                    //                console.log('(DRESS_IMAGES.JS) Mostrando los vestidos añadidos.', numImagenes);
+                    //                for (var j = 0; j < numImagenes; j++) {
+                    //                    console.log('Cargando imagenes: ', req.files[j].path);
 
-                            console.log('Cargando imagenes: ', req.files[i].path)
+                    /*          cloudinary.uploader.upload(req.file[i].path, function (result) {
+                                var fileUrl = result.url
+                                // var fileSecureUrl = result.secure_url
 
-                            dress.save().then(function (dressNew) {
-                              console.log('(DRESS_IMAGES.JS) Vestido grabado correctamente.')
-                              res.render('dresses/dress_images', {
-                                pageTitle: 'Agregar imagen al vestido: ' + dress.title,
-                                pageName: 'dress_images',
-                                sessionUser: req.session.userLoged,
-                                errors: null,
-                                dress: dressNew
-                              })
-                            }).catch(function (errors) {
-                              console.log('(DRESS_IMAGES.JS) Vestido no se grabo.')
-                              res.render('dresses/dress_images', {
-                                pageTitle: 'Agregar imagen al vestido: ' + dress.title,
-                                pageName: 'dress_images',
-                                sessionUser: req.session.userLoged,
-                                errors: errors,
-                                dress: dress
-                              })
-                            })
-                          }) */
-                //                }
-                res.send('(DRESS_IMAGES.JS) Grabado todo.');
-            }).catch(function(errors) {
-                console.log('(DRESS_IMAGES.JS) ERROR en la busqueda del vestido', errors);
-                res.send('(DRESS_IMAGES.JS) ERROR en la busqueda del vestido. '.errors);
-            });
+                                // dress.image = fileUrl
+                                dress.stateId = 1
+                                console.log('********* Vestido a grabar: ' + dress)
+
+                                console.log('Cargando imagenes: ', req.files[i].path)
+
+                                dress.save().then(function (dressNew) {
+                                console.log('(DRESS_IMAGES.JS) Vestido grabado correctamente.')
+                                res.render('dresses/dress_images', {
+                                    pageTitle: 'Agregar imagen al vestido: ' + dress.title,
+                                    pageName: 'dress_images',
+                                    sessionUser: req.session.userLoged,
+                                    errors: null,
+                                    dress: dressNew
+                                })
+                                }).catch(function (errors) {
+                                console.log('(DRESS_IMAGES.JS) Vestido no se grabo.')
+                                res.render('dresses/dress_images', {
+                                    pageTitle: 'Agregar imagen al vestido: ' + dress.title,
+                                    pageName: 'dress_images',
+                                    sessionUser: req.session.userLoged,
+                                    errors: errors,
+                                    dress: dress
+                                })
+                                })
+                            }) */
+                    //                }
+                    res.send('(DRESS_IMAGES.JS) Grabado todo.');
+                }).catch(function(errors) {
+                    console.log('(DRESS_IMAGES.JS) ERROR en la busqueda del vestido', errors);
+                    res.send('(DRESS_IMAGES.JS) ERROR en la busqueda del vestido. '.errors);
+                });
+            } else {
+                console.log('(DRESS_IMAGES.JS) ERROR el id del vestido no encontrado. ');
+                res.send('(DRESS_IMAGES.JS) ERROR el id del vestido no encontrado. ');
+            }
         } else {
-            console.log('(DRESS_IMAGES.JS) ERROR el id del vestido no encontrado. ');
-            res.send('(DRESS_IMAGES.JS) ERROR el id del vestido no encontrado. ');
+            console.log('(DRESS_IMAGES.JS) ERROR ningun archivo pasado.');
+            res.send('(DRESS_IMAGES.JS) ERROR ningun archivo pasado.');
         }
-    } else {
-        console.log('(DRESS_IMAGES.JS) ERROR ningun archivo pasado.');
-        res.send('(DRESS_IMAGES.JS) ERROR ningun archivo pasado.');
     }
-});
+);
 
 module.exports = router;
