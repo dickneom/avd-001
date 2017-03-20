7f9f8451a9e9cfc9c9b6e3bdc207387b45b7a753
DickNeoM <dickneom@yahoo.com>
2017-03-20 08:39:40 -0500

    Cambiando sintaxis y subiendo imagenes

--- models/dress_photos.js
+++ models/dress_photos.js
@@ -1,32 +1,32 @@
 // /models.dresses_photos.js
 
-module.exports = function (sequelize, DataTypes) {
-  var Photo = sequelize.define('Photo', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    dressId: {
-      type: DataTypes.TEXT,
-      field: 'dress_id'
-    },
-    photo_url: {
-      type: DataTypes.TEXT
-    },
-    photo: {
-      type: DataTypes.BLOB
-    },
-    description: {
-      type: DataTypes.TEXT
-    }
-  }, {
-    tableName: 'dresses_photos',
-    timestamps: true,
-    underscored: true,
-    paranoid: true
-    // aqui faltan las relaciones
-  })
+module.exports = function(sequelize, DataTypes) {
+    var Photo = sequelize.define('Photo', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        dressId: {
+            type: DataTypes.TEXT,
+            field: 'dress_id'
+        },
+        photo_url: {
+            type: DataTypes.TEXT
+        },
+        photo: {
+            type: DataTypes.BLOB
+        },
+        description: {
+            type: DataTypes.TEXT
+        }
+    }, {
+        tableName: 'dresses_photos',
+        timestamps: true,
+        underscored: true,
+        paranoid: true
+            // aqui faltan las relaciones
+    });
 
-  return Photo
-}
+    return Photo;
+};
